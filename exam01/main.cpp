8bdc98d5bc9de7f2da243a71e9b89221b925d7a8
ININBALA <k94191@gmail.com>
2020-04-01 17:15:57 +0800

    add PWM

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -6,17 +6,34 @@
 
 #include "uLCD_4DGL.h"
 
-
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
-
+PwmOut PWM1(D6);
+AnalogIn Ain(A0);
 
 int main()
 
-{
-
-    // basic printf demo = 16 by 18 characters on screen
-
+{   
     uLCD.printf("\n106061232\n"); //Default Green on black text
     uLCD.rectangle(40, 40, 50, 50, 0XFFFFFF);
-
+    int flag = 0;
+    while(1){
+        PWM1.period(0.001);
+        if(flag ==0){
+            PWM1 = PWM1 + 0.1;
+            wait(0.1);
+            if(PWM1 == 1){
+                flag = 1;
+            }
+        }
+        else{
+            PWM1 = PWM1 - 0.1;
+            wait(0.1);
+            if(PWM1 == 0){
+                flag = 0;
+            }
+        }
+    }
+        
+    
 }
+
