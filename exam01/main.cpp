48fd71c78f9ed99d2be858dd80f5a46fa08e6ca9
ININBALA <k94191@gmail.com>
2020-04-01 17:44:44 +0800

    add  main.cpp

--- exam01/main.cpp
+++ exam01/main.cpp
@@ -7,33 +7,53 @@
 #include "uLCD_4DGL.h"
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
-PwmOut PWM1(D6);
-AnalogIn Ain(A0);
+AnalogOut PWM1(D7);
+Serial pc( USBTX, USBRX);
 
 int main()
-
 {   
+    float PWMdata = 0;
     uLCD.printf("\n106061232\n"); //Default Green on black text
     uLCD.rectangle(40, 40, 50, 50, 0XFFFFFF);
     int flag = 0;
+    float k = 0;
+    flaot time;
+
     while(1){
-        PWM1.period(0.001);
-        if(flag ==0){
-            PWM1 = PWM1 + 0.1;
-            wait(0.1);
-            if(PWM1 == 1){
-                flag = 1;
+        if(flag == ){
+            if(time <= 0.1){
+                PWM1 = 0
+                wait(0.001);
+                PWM1 = k;
+                wait(0.001);
+                time = time + 0.002;
+                if(k == 1){
+                    flag = 1;
+                }
+            }
+            else {
+                k = k + 0.1;
             }
         }
         else{
-            PWM1 = PWM1 - 0.1;
-            wait(0.1);
-            if(PWM1 == 0){
-                flag = 0;
+            if(time <= 0.1){
+                PWM1 = 0
+                wait(0.001);
+                PWM1 = k;
+                wait(0.001);
+                time = time + 0.002;
+                if(k == 0){
+                    flag = 0;
+                }
+            }
+            else {
+                k = k - 0.1;
             }
         }
-    }
-        
-    
+
+
+        }
+
+    }    
 }
 
